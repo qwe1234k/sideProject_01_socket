3054c57a1a1d7419234105eb904b9ecff6e2244b
qwe1234k <bbyl0124@gmail.com>
2022-08-06 11:04:28 +0900

    add userNickName

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const helmet = require("helmet");
 const http = require("http");
 const hpp = require("hpp");
 const { Server } = require("socket.io");
+const _ = require("lodash");
 //각종 미들웨어
 app.use(express.json());
 app.use(express.urlencoded());
@@ -30,9 +31,37 @@ const io = new Server(server, {
         origin: "http://localhost:3000",
     },
 });
+// 닉네임 배열
+const animalName = [
+    "나무늘보",
+    "팬더",
+    "개미핥기",
+    "고라니",
+    "토끼",
+    "강아지",
+    "고슴도치",
+    "비버",
+    "원숭이",
+    "송아지",
+];
+const adjectiveName = [
+    "부지런한",
+    "좀더 자고싶은",
+    "힘이 넘치는",
+    "긴장되는",
+    "신이난",
+    "궁금한게 많은",
+    "흡족한",
+    "통쾌한",
+    "섭섭한",
+    "덤덤한",
+];
 // 소켓 로직
 io.on("connection", (socket) => {
-    console.log(`user connected: ${socket.id}`);
+    const combinedName = _.sample(adjectiveName) + " " + _.sample(animalName);
+    socket.nickName = combinedName;
+    socket.emit("user_nickName", socket.nickName);
+    console.log(`user connected: ${socket.id}, user nickName: ${socket.nickName}`);
     // 1. Room 입장 이벤트 처리
     socket.on("join_room", (roomId) => {
         socket.join(roomId);
@@ -41,10 +70,11 @@ io.on("connection", (socket) => {
     // 2. Room에 메세지 전송 이벤트 처리
     socket.on("send_message", (data) => __awaiter(void 0, void 0, void 0, function* () {
         socket.to(data.room).emit("receive_message", data);
-        // data = { room: "2208267975_2208267975",
-        //          author: "윤지", authorId: "123456",
-        //          targetAuthor: "태성", targetAuthorId: "654321",
-        //          message: "d", time: "16:46" }
+        /* data = { room: "2208267975_2208267975",
+                  author: "윤지", authorId: "123456",
+                  targetAuthor: "태성", targetAuthorId: "654321",
+                  message: "d", time: "16:46" }
+        */
         console.log("이게 메세지 일까요??", data);
     }));
     // 3. Socket 연결 끊기 이벤트 처리
